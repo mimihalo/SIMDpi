f9d1164967add8ac278fea8d6b62fabbc57059e1
mimihalo <mimihalo@miko.tw>
2015-11-05 16:01:51 +0800

    modify main to get the performance

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdint.h>
 #include IMPL
 
 int main()
 {
-    compute_pi(0x8000000);
-    return 0;
+	uint64_t begin,end;
+	unsigned long long int dt;
+    double pi,times;
+	FILE *fp=fopen("result.txt","w");
+	
+	dt=0x100000;
+	for(dt=0x100000;dt<=0x40000000;dt<<=1)
+	{
+		begin=clock();
+		pi=compute_pi(dt);
+		end=clock();
+		times = (double)(end - begin) / (CLOCKS_PER_SEC/1000);
+		printf("dt = %lluM\n",(dt/(1<<20)));
+		printf("pi = %f\n",pi);
+		printf("time = %f\n",times);
+		fprintf(fp,"%llu\t%f\t%f\n",(dt/(1<<20)),pi,times);
+	}
+	
+	return 0;
 }
