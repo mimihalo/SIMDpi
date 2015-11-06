fe852154778b2690bc3ad7c13729ebcdddb7292b
mimihalo <mimihalo@miko.tw>
2015-11-06 13:21:33 +0800

    modify main to get error rate

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdint.h>
+#include <math.h>
 #include IMPL
 
 int main()
@@ -8,6 +9,8 @@ int main()
 	uint64_t begin,end;
 	unsigned long long int dt;
     double pi,times;
+	double rpi=acos(-1.0);
+	double err;
 	FILE *fp=fopen("result.txt","w");
 	
 	dt=0x100000;
@@ -17,10 +20,12 @@ int main()
 		pi=compute_pi(dt);
 		end=clock();
 		times = (double)(end - begin) / (CLOCKS_PER_SEC/1000);
+		err = (pi-rpi)/rpi;
 		printf("dt = %lluM\n",(dt/(1<<20)));
-		printf("pi = %f\n",pi);
+		printf("pi = %.10f\n",pi);
 		printf("time = %f\n",times);
-		fprintf(fp,"%llu\t%f\t%f\n",(dt/(1<<20)),pi,times);
+		printf("error rate = %.10e\n",err);
+		fprintf(fp,"%llu\t%.10f\t%f\t%.16f\n",(dt/(1<<20)),pi,times,err);
 	}
 	
 	return 0;
